9f367a40d0a3c27d24ed340e3dd75228272a667a
Anders Bjerner <abjerner@skybrud.dk>
2023-08-13 16:52:05 +0200

    Improved the logic for converting Skybrud Image Picker values

--- src/Limbo.Umbraco.Migrations/Models/MediaPicker/MediaPickerList.cs
+++ src/Limbo.Umbraco.Migrations/Models/MediaPicker/MediaPickerList.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Umbraco.Cms.Core.Models;
 
 namespace Limbo.Umbraco.Migrations.Models.MediaPicker {
 
@@ -21,12 +23,34 @@ namespace Limbo.Umbraco.Migrations.Models.MediaPicker {
             _items.Add(item);
         }
 
+        public MediaPickerList AddItem(Guid itemKey, Guid mediaKey) {
+            Add(new MediaPickerItem(itemKey, mediaKey));
+            return this;
+        }
+
+        public MediaPickerList AddItem(Guid itemKey, IMedia media) {
+            Add(new MediaPickerItem(itemKey, media.Key));
+            return this;
+        }
+
         public IEnumerator<MediaPickerItem> GetEnumerator() {
             return _items.GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator() {
             return GetEnumerator();
+        }
+
+        public static MediaPickerList Create() {
+            return new MediaPickerList();
+        }
+
+        public static MediaPickerList CreateWithItem(Guid itemKey, Guid mediaKey) {
+            return new MediaPickerList().AddItem(itemKey, mediaKey);
+        }
+
+        public static MediaPickerList CreateWithItem(Guid itemKey, IMedia media) {
+            return new MediaPickerList().AddItem(itemKey, media);
         }
 
     }
