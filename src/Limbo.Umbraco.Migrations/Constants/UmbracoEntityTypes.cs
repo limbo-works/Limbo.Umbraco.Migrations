a4b50859007cd65cd82adeea0a5f35aa27b04f72
Anders Bjerner <abjerner@skybrud.dk>
2023-08-24 10:42:21 +0200

    Introduced "UmbracoEntityTypes" class with constants
    Umbraco's "Constants" class can be a bit tiresome to access when there is also a namespace in the project called "Constants", so the "UmbracoEntityTypes" class now contains a few constants also found in the Umbraco's "Constants.UdiEntityType" nested class.

---
+++ src/Limbo.Umbraco.Migrations/Constants/UmbracoEntityTypes.cs
@@ -0,0 +1,16 @@
+﻿namespace Limbo.Umbraco.Migrations.Constants {
+
+    public class UmbracoEntityTypes {
+
+        /// <summary>
+        /// Alias of <see cref="Document"/>.
+        /// </summary>
+        public const string Content = global::Umbraco.Cms.Core.Constants.UdiEntityType.Document;
+
+        public const string Document = global::Umbraco.Cms.Core.Constants.UdiEntityType.Document;
+
+        public const string Media = global::Umbraco.Cms.Core.Constants.UdiEntityType.Media;
+
+    }
+
+}
