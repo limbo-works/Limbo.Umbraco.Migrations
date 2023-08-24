8c6f96e63ee65307f996a4b295ef62b006490954
Anders Bjerner <abjerner@skybrud.dk>
2023-08-24 10:42:52 +0200

    Added constants for various known property editor aliases

---
+++ src/Limbo.Umbraco.Migrations/Constants/PropertyEditorAliases.cs
@@ -0,0 +1,26 @@
+﻿namespace Limbo.Umbraco.Migrations.Constants {
+
+    /// <summary>
+    /// Static class with constants for various legacy property editor aliases.
+    /// </summary>
+    public static class PropertyEditorAliases {
+
+        public static class NuPickers {
+
+            public const string DotNetCheckBoxPicker = "nuPickers.DotNetCheckBoxPicker";
+
+        }
+
+        public static class Skybrud {
+
+            public const string ImagePicker = "Skybrud.ImagePicker";
+
+            public const string LinkPicker = "Skybrud.LinkPicker";
+
+        }
+
+        public const string Archetype = "Imulus.Archetype";
+
+    }
+
+}
