42f169dfbf1ef6b3cf0a565f46783a565351ca08
Anders Bjerner <abjerner@skybrud.dk>
2024-12-10 12:22:43 +0100

    Added models to represent the new RTE data structure in Umbraco 13

---
+++ src/Limbo.Umbraco.Migrations/Models/Rte/RteBlocks.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Limbo.Umbraco.Migrations.Models.Rte;
+
+/// <summary>
+/// Represents the blocks part of <see cref="RteModel"/>.
+/// </summary>
+public class RteBlocks {
+
+    [JsonProperty("contentData")]
+    public IReadOnlyList<object> ContentData { get; } = [];
+
+    [JsonProperty("settingsData")]
+    public IReadOnlyList<object> SettingsData { get; } = [];
+
+}
---
+++ src/Limbo.Umbraco.Migrations/Models/Rte/RteModel.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace Limbo.Umbraco.Migrations.Models.Rte;
+
+/// <summary>
+/// Class representing the RTE model in Umbraco 13.
+/// </summary>
+public class RteModel {
+
+    [JsonProperty("markup")]
+    public string Markup { get; set; }
+
+    [JsonProperty("blocks")]
+    public RteBlocks Blocks { get; } = new();
+
+    public RteModel(string markup) {
+        Markup = markup;
+    }
+
+}
