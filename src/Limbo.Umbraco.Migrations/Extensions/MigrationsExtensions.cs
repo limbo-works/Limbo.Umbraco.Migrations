b494cf38495259e18fa411fe45eb00e16b7b65ea
Anders Bjerner <abjerner@skybrud.dk>
2025-02-04 11:07:27 +0100

    Added additional functionality for UrlPickerList

--- src/Limbo.Umbraco.Migrations/Extensions/MigrationsExtensions.cs
+++ src/Limbo.Umbraco.Migrations/Extensions/MigrationsExtensions.cs
@@ -1,8 +1,10 @@
-﻿using Limbo.Umbraco.Migrations.Constants;
+﻿using System.Linq;
+using Limbo.Umbraco.Migrations.Constants;
+using Limbo.Umbraco.Migrations.Exceptions;
+using Limbo.Umbraco.Migrations.Models.MediaPicker;
 using Limbo.Umbraco.Migrations.Models.UrlPicker;
 using Limbo.Umbraco.MigrationsClient.Models.Content;
 using Limbo.Umbraco.MigrationsClient.Models.Umbraco;
-using Skybrud.Essentials.Exceptions;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Extensions;
 
@@ -16,18 +18,56 @@ public static class MigrationsExtensions {
         return udi is null ? null : new UmbracoGuidUdi(udi.EntityType, udi.Guid);
     }
 
-    public static UrlPickerList AddContent(this UrlPickerList list, LegacyContent content, string? target = null, string? queryString = null) {
+
+    /// <summary>
+    /// Adds a new content item based on the specified <paramref name="content"/>.
+    /// </summary>
+    /// <param name="list">The list.</param>
+    /// <param name="content">The content item the list should be based.</param>
+    /// <param name="target">The target of the URL item. Default is <see langword="null"/>.</param>
+    /// <param name="queryString">The query string, if any.</param>
+    /// <returns><paramref name="list"/> - useful for method chaining.</returns>
+    public static TList AddContent<TList>(this TList list, LegacyContent content, string? target = null, string? queryString = null) where TList : UrlPickerList {
         list.Add(UrlPickerItem.CreateContentItem(content.Name, new UmbracoGuidUdi(UmbracoEntityTypes.Content, content.Key), content.Url, target, queryString));
         return list;
     }
 
-    public static UrlPickerList AddMedia(this UrlPickerList list, IMedia media, string? target = null, string? queryString = null) {
-        string url = media.GetValue<string>("umbracoFile") ?? throw new BjernerSaysNoException();
+    /// <summary>
+    /// Adds a new media item based on the specified <paramref name="media"/>.
+    /// </summary>
+    /// <param name="list">The list.</param>
+    /// <param name="media">The media item the list should be based.</param>
+    /// <param name="target">The target of the URL item. Default is <see langword="null"/>.</param>
+    /// <param name="queryString">The query string, if any.</param>
+    /// <returns><paramref name="list"/> - useful for method chaining.</returns>
+    public static TList AddMedia<TList>(this TList list, IMedia media, string? target = null, string? queryString = null) where TList : UrlPickerList {
+        // TODO: can we get the URL this way for images? (value might be JSON)
+        string url = media.GetValue<string>("umbracoFile") ?? throw new MigrationsException($"Failed determening URL for media with key '{media.Key}'.");
         list.Add(UrlPickerItem.CreateMediaItem(media.Name!, media.GetUdi(), url, target, queryString));
         return list;
     }
 
-    public static UrlPickerList? NullIfEmpty(this UrlPickerList? list) {
+    /// <summary>
+    /// Adds a new external item based on the specified <paramref name="url"/>.
+    /// </summary>
+    /// <typeparam name="TList"></typeparam>
+    /// <param name="list"></param>
+    /// <param name="url">The URL the item should be based on.</param>
+    /// <param name="name">The name of the item. If not specified, the name will be the same as <paramref name="url"/>.</param>
+    /// <param name="target">The target of the URL item. Default is <c>_blank</c>.</param>
+    /// <param name="queryString">The query string, if any.</param>
+    /// <returns><paramref name="list"/> - useful for method chaining.</returns>
+    public static TList AddExternal<TList>(this TList list, string url, string? name = null, string? target = "_blank", string? queryString = null) where TList : UrlPickerList {
+        list.Add(new UrlPickerItem(LinkType.External, name ?? url, null, url, target, queryString));
+        return list;
+    }
+
+    /// <summary>
+    /// Returns <see langword="null"/> if <paramref name="list"/> is also <see langword="null"/>, otherwise, <paramref name="list"/> is returned unmodified.
+    /// </summary>
+    /// <param name="list">The list.</param>
+    /// <returns><paramref name="list"/> if not <see langword="null"/>; otherwise, <see langword="null"/>.</returns>
+    public static TList? NullIfEmpty<TList>(this TList? list) where TList : UrlPickerList {
         return list is null || list.Count == 0 ? null : list;
     }
 
--- src/Limbo.Umbraco.Migrations/Models/UrlPicker/UrlPickerList.cs
+++ src/Limbo.Umbraco.Migrations/Models/UrlPicker/UrlPickerList.cs
@@ -1,21 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Limbo.Umbraco.Migrations.Extensions;
+using Limbo.Umbraco.MigrationsClient.Models.Content;
+using Umbraco.Cms.Core.Models;
 
 namespace Limbo.Umbraco.Migrations.Models.UrlPicker;
 
+/// <summary>
+/// Class representing a URL picker list.
+/// </summary>
 public class UrlPickerList : IReadOnlyList<UrlPickerItem> {
 
     private readonly List<UrlPickerItem> _items = [];
 
+    #region Properties
+
     public int Count => _items.Count;
 
     public UrlPickerItem this[int index] => _items[index];
+
+    #endregion
+
+    #region Constructors
 
     public UrlPickerList() { }
 
     public UrlPickerList(UrlPickerItem item) {
         Add(item);
     }
+
+    #endregion
+
+    #region Member methods
 
     public void Add(UrlPickerItem item) {
         _items.Add(item);
@@ -28,5 +45,48 @@ public class UrlPickerList : IReadOnlyList<UrlPickerItem> {
     IEnumerator IEnumerable.GetEnumerator() {
         return GetEnumerator();
     }
+
+    #endregion
+
+    #region Static methods
+
+    /// <summary>
+    /// Creates and returns a new <see cref="UrlPickerList"/> instance containing an item for the specified <paramref name="content"/>.
+    /// </summary>
+    /// <param name="content">The content item the list should be based.</param>
+    /// <param name="target">The target of the URL item. Default is <see langword="null"/>.</param>
+    /// <param name="queryString">The query string, if any.</param>
+    /// <returns>An instance of <see cref="UrlPickerList"/>, or <see langword="null"/> if <paramref name="content"/> is <see langword="null"/>.</returns>
+    [return: NotNullIfNotNull(nameof(content))]
+    public static UrlPickerList? CreateFromContent(LegacyContent? content, string? target = null, string? queryString = null) {
+        return content is null ? null : new UrlPickerList().AddContent(content, target, queryString);
+    }
+
+    /// <summary>
+    /// Creates and returns a new <see cref="UrlPickerList"/> instance containing an item for the specified <paramref name="media"/>.
+    /// </summary>
+    /// <param name="media">The content item the list should be based.</param>
+    /// <param name="target">The target of the URL item. Default is <see langword="null"/>.</param>
+    /// <param name="queryString">The query string, if any.</param>
+    /// <returns>An instance of <see cref="UrlPickerList"/>, or <see langword="null"/> if <paramref name="media"/> is <see langword="null"/>.</returns>
+    [return: NotNullIfNotNull(nameof(media))]
+    public static UrlPickerList? CreateFromMedia(IMedia? media, string? target = null, string? queryString = null) {
+        return media is null ? null : new UrlPickerList().AddMedia(media, target, queryString);
+    }
+
+    /// <summary>
+    /// Creates and returns a new <see cref="UrlPickerList"/> instance containing an external item for the specified <paramref name="url"/>.
+    /// </summary>
+    /// <param name="url">The external URL item the list should be based.</param>
+    /// <param name="name">The name of the item. If not specified, the name will be the same as <paramref name="url"/>.</param>
+    /// <param name="target">The target of the URL item. Default is <c>_blank</c>.</param>
+    /// <param name="queryString">The query string, if any.</param>
+    /// <returns>An instance of <see cref="UrlPickerList"/>, or <see langword="null"/> if <paramref name="url"/> is <see langword="null"/> or white space.</returns>
+    [return: NotNullIfNotNull(nameof(url))]
+    public static UrlPickerList? CreateFromExternal(string? url, string? name = null, string? target = "_blank", string? queryString = null) {
+        return string.IsNullOrWhiteSpace(url) ? null : new UrlPickerList().AddExternal(url, name, target, queryString);
+    }
+
+    #endregion
 
 }
